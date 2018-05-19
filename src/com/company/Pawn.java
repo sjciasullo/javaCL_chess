689f704c2e91fb41ebe77e277f70799ec3e972f1
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-19 17:21:59 -0400

    complete untested canMoveToPosition

--- src/com/company/Pawn.java
+++ src/com/company/Pawn.java
@@ -1,26 +1,41 @@
 package com.company;
 
 public class Pawn extends Piece{
-    private boolean isFirstMove = true;
+  private boolean isFirstMove = true;
 
-    Pawn(String team, int[] position){
-        super(team, position);
-    }
+  Pawn(String team, int[] position){
+    super(team, position);
+  }
 
-    public String getBoardName(){
-        return "(" + this.getTeam().charAt(0) + ")" + " pawn";
-    }
+  public String getBoardName(){
+    return "(" + this.getTeam().charAt(0) + ")" + " pawn";
+  }
 
-    public boolean canMoveToPosition(String[][] board, int[] newPosition){
-        String occupant = board[ newPosition[0] ][ newPosition[1] ];
+  public boolean canMoveToPosition(String[][] board, int[] newPosition){
+    String occupant = board[ newPosition[Piece.ROW] ][ newPosition[Piece.COLUMN] ];
+    int teamFactor = this.getTeam().equals("black") ? 1 : -1;
+    int newRow = newPosition[Piece.ROW];
+    int newColumn = newPosition[Piece.COLUMN];
+    int currentRow = this.getPosition()[Piece.ROW];
+    int currentColumn = this.getPosition()[Piece.COLUMN];
 
-        // Check if position is empty or held by other team. Ex. "(w) bishop" and black
-        if(occupant.isEmpty()){
-            // if same x value, check y value with account for team
-                // if difference is 1 then it is one away and unoccupied
-        } else if(occupant.charAt(1) != this.getTeam().charAt(0)){
-            // needs to be diagonal and in correct direction
+    // Check if position is empty or held by other team. Ex. "(w) bishop" and black
+    if(occupant.isEmpty()){
+      // if same x value, check y value with account for team
+      // black pawn increases rows, white pawn decreases rows
+      if(newColumn == currentColumn){
+        if(newRow == currentRow + teamFactor ||
+           newRow == currentRow + (teamFactor * 2) && this.isFirstMove){
+          return true;
         }
-        return false;
+      }
+    } else if(occupant.charAt(1) != this.getTeam().charAt(0)){
+      // needs to be diagonal and in correct direction
+      if(newRow == currentRow + teamFactor &&
+         (newColumn == currentColumn + 1 || newColumn == currentColumn - 1) ){
+        return true;
+      }
     }
+    return false;
+  }
 }
