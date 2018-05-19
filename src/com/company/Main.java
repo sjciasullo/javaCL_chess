35c4fb546eb2f1f83a78d3a06782e0fbfc712600
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-19 18:52:11 -0400

    edit tabbing format

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// TESTING CLASS METHODS AND PROPERTIES
+  public static void main(String[] args) {
+    // TESTING CLASS METHODS AND PROPERTIES
 
-        // PAWN
-        int[] position = {1,2};
-        Pawn pawn = new Pawn("black", position);
-        System.out.println(pawn.getBoardName());
+    // PAWN
+    int[] position = {1,2};
+    Pawn pawn = new Pawn("black", position);
+    System.out.println(pawn.getBoardName());
 
-    }
+  }
 }
