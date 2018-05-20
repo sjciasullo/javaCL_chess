a06ed15f9a3a5e1cfb80b992ddb15f4c81b4a8d6
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-20 18:30:22 -0400

    implement Rook.getBoardName()

--- src/com/company/Rook.java
+++ src/com/company/Rook.java
@@ -4,4 +4,8 @@ public class Rook extends Piece{
   Rook(String team, Coordinate position){
     super(team, position);
   }
+
+  public String getBoardName(){
+    return "(" + this.getTeam().charAt(0) + ")" + " rook";
+  }
 }
