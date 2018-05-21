8c1411a63ddbf452bad94011ae74c8dd0c44689e
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-20 20:03:15 -0400

    create King.isValidMove

--- src/com/company/King.java
+++ src/com/company/King.java
@@ -4,4 +4,21 @@ public class King extends Piece{
   King(String team, Coordinate position){
     super(team, position);
   }
+
+  public String getBoardName() {
+    return "(" + this.getTeam().charAt(0) + ")" + " king";
+  }
+
+  public boolean isValidMove(String[][] board, Coordinate newPosition){
+    String occupant = board[newPosition.getRow()][newPosition.getColumn()];
+    int rowDifference = newPosition.getRow() - this.getPosition().getRow();
+    int columnDifference = newPosition.getColumn() - this.getPosition().getColumn();
+
+    if(occupant.isEmpty() || occupant.charAt(1) != this.getTeam().charAt(0)){
+      if(Math.abs(columnDifference) <= 1 && Math.abs(rowDifference) <= 1){
+        return true;
+      }
+    }
+    return false;
+  }
 }
