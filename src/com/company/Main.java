153f918bd48314951dce81e8bcd2675e34155b44
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-21 07:19:57 -0400

    remove Pawn test from main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,6 @@ package com.company;
 public class Main {
 
   public static void main(String[] args) {
-    // TESTING CLASS METHODS AND PROPERTIES
-
-    // PAWN
-    Coordinate position = new Coordinate(1,2);
-    Pawn pawn1 = new Pawn("black", position);
-    System.out.println(pawn1.getBoardName());
-
     // BEGIN GAMESTATE
     GameState game = new GameState();
     game.runGame();
