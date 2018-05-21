776b321b85c6d0421b35eee1de1994c90be93a66
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-20 20:54:39 -0400

    calculate valid moves for bishop

--- src/com/company/Bishop.java
+++ src/com/company/Bishop.java
@@ -8,4 +8,22 @@ public class Bishop extends Piece {
   public String getBoardName(){
     return "(" + this.getTeam().charAt(0) + ")" + " bishop";
   }
+
+  public boolean isValidMove(String[][] board, Coordinate newPosition) {
+    String occupant = board[newPosition.getRow()][newPosition.getColumn()];
+    if (occupant.isEmpty() || occupant.charAt(1) != this.getTeam().charAt(0)) {
+      // first must check vertical line to prevent divide by zero
+      if (newPosition.getColumn() == this.getPosition().getColumn()) {
+        return false;
+      }
+      //calculate slope
+      int changeY = newPosition.getRow() - this.getPosition().getRow();
+      int changeX = newPosition.getColumn() - this.getPosition().getColumn();
+      float slope = changeY / changeX;
+      if (Math.abs(slope) == 1) {
+        return Piece.isClearPath(board, this.getPosition(), newPosition);
+      }
+    }
+    return false;
+  }
 }
