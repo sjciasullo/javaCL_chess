4ae35d4c484a60f195b471dea35d5d8a8842eb50
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-20 20:51:20 -0400

    create boardname for bishop

--- src/com/company/Bishop.java
+++ src/com/company/Bishop.java
@@ -4,4 +4,8 @@ public class Bishop extends Piece {
   Bishop(String team, Coordinate position){
     super(team, position);
   }
+
+  public String getBoardName(){
+    return "(" + this.getTeam().charAt(0) + ")" + " bishop";
+  }
 }
