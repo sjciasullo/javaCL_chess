912f80f08306dc9a96511cb4f5add6c9d5d5a4e8
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-20 20:13:04 -0400

    implement Queen.getBoardName()

--- src/com/company/Queen.java
+++ src/com/company/Queen.java
@@ -4,4 +4,10 @@ public class Queen extends Piece {
   Queen(String team, Coordinate position){
     super(team, position);
   }
+
+  public String getBoardName(){
+    return "(" + this.getTeam().charAt(0) + ")" + " queen";
+  }
+
+  
 }
